5c53b76c6636756e211aa2c3331dad22b20f5184
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2023-12-06 18:02:55 +0530

    react router learn

---
+++ learn-react-router-dom/src/Layout.jsx
@@ -0,0 +1,11 @@
+import {Outlet} from 'react-router-dom';
+import { Footer, Header } from './components';
+function Layout() {
+    return (
+    <>
+        <Header />
+            <Outlet />
+        <Footer />
+    </>);
+}
+export default Layout;
---
+++ learn-react-router-dom/src/components/index.js
@@ -0,0 +1,17 @@
+import About from "./About";
+import ContactUs from "./ContactUs";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Login from "./Login";
+import Profile from "./Profile";
+
+export {
+    About,
+    ContactUs,
+    Footer,
+    Header,
+    Home,
+    Login,
+    Profile
+}
